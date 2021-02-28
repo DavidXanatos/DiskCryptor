0675bcbab64b273ac26bbfe2bdbcbe1b15929714
Fabian Otto <fabian.otto@fbno.de>
2021-02-28 21:29:14 +0100

    Generalize EFI loader build scripts
    - Support out-of-source build
    - Support Visual Studio 2010-2019
    - Add build documentation

--- DcsPkg/OpenEnv.cmd
+++ DcsPkg/OpenEnv.cmd
@@ -1,19 +1,17 @@
 @echo off
+pushd %~dp0
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
-
-call "..\edksetup.bat"
-
-set PATH=%PATH%;C:\nasm
+call setenv.bat
 
 echo ***********************************************
 echo *  
-echo *  dcs_bld.bat X64 VS2015
-echo *  dcs_bld.bat X64Rel VS2015
+echo *  dcs_bld.bat X64
+echo *  dcs_bld.bat X64Rel
 echo *  
-echo *  dcs_bld.bat IA32 VS2015
-echo *  dcs_bld.bat IA32rel VS2015
+echo *  dcs_bld.bat IA32
+echo *  dcs_bld.bat IA32Rel
 echo *  
 echo. 
 
 cmd.exe
+popd
