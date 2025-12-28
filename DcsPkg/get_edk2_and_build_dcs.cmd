0483a526f4a36fe271d517eff8fccba7267f9612
DavidXanatos <xanatosdavid@gmail.com>
2025-12-28 11:04:04 +0100

    Update get_edk2_and_build_dcs.cmd

--- DcsPkg/get_edk2_and_build_dcs.cmd
+++ DcsPkg/get_edk2_and_build_dcs.cmd
@@ -19,6 +19,10 @@ dir %NASM_PREFIX%
 
 call setenv.bat
 
+REM fix
+REM \edk2\CryptoPkg\Library\OpensslLib\openssl\crypto\bn\bn_gcd.c(659): error C2220: the following warning is treated as an error
+REM \edk2\CryptoPkg\Library\OpensslLib\openssl\crypto\bn\bn_gcd.c(659): warning C4319: '~': zero extending 'unsigned int' to 'unsigned __int64' of greater size
+REM \edk2\CryptoPkg\Library\OpensslLib\openssl\crypto\bn\bn_gcd.c(671): warning C4319: '~': zero extending 'unsigned int' to 'unsigned __int64' of greater size
 set CL=/wd4319 %CL%
 
 call dcs_bld.bat X64Rel
