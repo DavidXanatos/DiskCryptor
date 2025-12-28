b304a637b89a518a83ab924ac2ed6809f3d68a12
DavidXanatos <xanatosdavid@gmail.com>
2025-12-28 10:34:07 +0100

    Uncommented git clone command for edk2 repository

--- DcsPkg/get_edk2_and_build_dcs.cmd
+++ DcsPkg/get_edk2_and_build_dcs.cmd
@@ -1,13 +1,11 @@
 cd %~dp0..\..
-REM git clone --branch edk2-stable202511 --recurse-submodules https://github.com/tianocore/edk2.git
-REM git clone --branch edk2-stable202511 --depth 1 --recurse-submodules --shallow-submodules https://github.com/tianocore/edk2.git
-
 dir
 
+REM git clone --branch edk2-stable202511 --recurse-submodules https://github.com/tianocore/edk2.git
+git clone --branch edk2-stable202511 --depth 1 --recurse-submodules --shallow-submodules https://github.com/tianocore/edk2.git
 cd edk2
 
 call edksetup.bat Rebuild
-
 
 mklink /J DcsPkg %~dp0
 cd DcsPkg
