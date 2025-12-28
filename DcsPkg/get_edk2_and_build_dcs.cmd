a47efd1b170c09013bcf42e647b8056e91d52340
DavidXanatos <xanatosdavid@gmail.com>
2025-12-28 10:26:26 +0100

    Update get_edk2_and_build_dcs.cmd

--- DcsPkg/get_edk2_and_build_dcs.cmd
+++ DcsPkg/get_edk2_and_build_dcs.cmd
@@ -1,16 +1,23 @@
-cd ..\..
+cd %~dp0..\..
 REM git clone --branch edk2-stable202511 --recurse-submodules https://github.com/tianocore/edk2.git
-git clone --branch edk2-stable202511 --depth 1 --recurse-submodules --shallow-submodules https://github.com/tianocore/edk2.git
+REM git clone --branch edk2-stable202511 --depth 1 --recurse-submodules --shallow-submodules https://github.com/tianocore/edk2.git
+
+dir
+
 cd edk2
 
 call edksetup.bat Rebuild
 
-mklink /J DcsPkg ..\DiskCryptor\DcsPkg
+
+mklink /J DcsPkg %~dp0
 cd DcsPkg
+
+dir
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
 
-set NASM_PREFIX=%~dp0..\..\DiskCryptor\DCrypt\tools\nasm\
+set NASM_PREFIX=%~dp0..\DCrypt\tools\nasm\
+dir %NASM_PREFIX%
 
 call setenv.bat
 
